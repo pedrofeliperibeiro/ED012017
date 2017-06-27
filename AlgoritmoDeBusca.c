7875449038ede8591a27a46023bc7543a4334258
Pedro Felipe De Oliveira Ribeiro <pedrofelipeo2378@gmail.com>
2017-06-27 15:08:06 -0300

    Create AlgoritmoDeBusca.c

---
+++ AlgoritmoDeBusca.c
@@ -0,0 +1,16 @@
+Pseudo-Código
+
+Receba Vetor
+Receba MAX
+Receba Elemento
+
+Flag = 0
+Para i=0 até MAx-1 Faça
+	Se Vetor[i] = Elemento então
+		Imprima "Achou"
+		Flag = 1
+	Fim Se
+Fim Para
+Se Flag = 0 Então 
+	Imprima "Elemento Não Encontrado"
+Fim Se
